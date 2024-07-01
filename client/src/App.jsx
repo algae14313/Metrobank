4c21e9930bfe32ed7d87ac7b3889856151500b45
lmnlad <lmnllad@gmail.com>
2024-07-01 08:02:41 +0800

    test

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,11 @@
-import { useState } from 'react'
-import './App.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import { RouterProvider } from 'react-router-dom'
+import Routes from './routes'
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-        
-    </>
-  )
-}
-
-export default App
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={Routes} />
+  </React.StrictMode>,
+)
