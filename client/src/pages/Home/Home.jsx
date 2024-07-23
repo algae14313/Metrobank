3d9dd8e3048c41eea818086f48ce9d8cbceb7797
lmnlad <lmnllad@gmail.com>
2024-07-23 11:31:57 +0800

    home

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -63,37 +63,6 @@ export default function Home() {
     return (
         <div className='w-full h-screen flex justify-start items-center flex-col bg-[#121212]'>
             <Header__Home />
-<<<<<<< HEAD
-            <Announcement color={`#000000`} content={announcement} />
-            <div className="overflow-hidden w-full h-full flex flex-col justify-evenly items-center relative">
-                <img src={SolanaPic} alt="Solana Pic" className='absolute top-[-2rem] left-[30rem] sm:left-[25rem] md:left-[20rem] lg:-left-[15rem] w-[20rem] h-[10rem] scale-[5]' />
-                <img src={SolanaPic} alt="Solana Pic" className='absolute top-[10rem] right-[-30rem] sm:right-[-25rem] md:right-[-20rem] lg:right-[-15rem] w-[20rem] h-[10rem] scale-[5]' />
-                {/* <div className="absolute w-full h-screen bg-[rgba(0,0,0,0.54)] flex justify-center items-center z-[2]">
-                    <h1 className='text-white text-[4rem] text-bold'>Maintenance break, we will be right back</h1>
-                </div> */}
-                <div className="w-full flex flex-col justify-center items-center gap-[1rem]">
-                    <h1 className='text-white text-[4rem] font-[600] text-center z-[1]'>
-                        Powerful for developers. <br /> Fast for everyone.
-                    </h1>
-                    <div className="w-full">
-                        <h1 className='text-[#87878e] text-center'>
-                            Bring blockchain to the people. UnionBank supports experiences <br /> for power users, new consumers, and everyone in between.
-                        </h1>
-                    </div>
-                    <div className="w-full flex justify-center items-center gap-[1rem] z-[1]">
-                        <Link to={`https://spiritual-wire-287.notion.site/UnionBank-API-Documentation-6a2928ba55e5442b91423fda3ebd8f78?pvs=4`}
-                            className='text-white px-[1.7rem] py-[.7rem] border-white border-[1px] rounded-3xl hover:bg-white hover:text-black'
-                            target='_blank'
-                        >
-                            READ DOCS
-                        </Link>
-                        <button onClick={sendMessage} className='text-white'>
-                            Test Broadcast
-                        </button>
-                    </div>
-                </div>
-            </div>
-=======
             {/* <div className="w-full h-full p-[5rem]">
                 <img src={test} alt="asd" className='w-full h-full object-cover' />
             </div> */}
@@ -131,7 +100,6 @@ export default function Home() {
                     ANO DAW ANO DAW ANO DAW ANO DAW ANO DAW 
                 </h1>
             </marquee> */}
->>>>>>> 5e3010578ff8f97f848176b4e9353934d9defc22
         </div>
     )
 }
